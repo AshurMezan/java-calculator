19736b779ab8b1ad1a2ff6301f2aac082b95425c
Lion 2517 <lion2517@yahoo.com>
2023-03-20 16:22:26 +0500

    Исправил ошибку в условиях

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -5,41 +5,42 @@ import java.util.Scanner; // Импортировал класс Scanner для 
 public class Main { //точка входа в документ
     public static void main(String[] args) { // основное тело класса
 
-        Scanner in = new Scanner(System.in); // создал новый класс in и дал ему метод
-        System.out.print("Напиши 1-e число.\n"); // сообщение к пользователю на ввод информации
-        int numb1 = in.nextInt(); // вот сюда эта информация сохраняется
-        System.out.print("Напиши 2-е число.\n"); // сообщение к пользователю на ввод информации
-        int numb2 = in.nextInt(); // вот сюда эта информация сохраняется
-        int resultOutcome; // создаю переменную результат
-        System.out.print("Действие: "); // Вывод сообщения на экран, что будем делать: делить или умножать
-        String action = in.nextLine(); // вот сюда мы помещаем символ математической операции
-        action = in.nextLine();
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Напиши 1-e число.\n"); // сообщение к пользователю на ввод информации
+            int numb1 = in.nextInt(); // вот сюда эта информация сохраняется
+            System.out.print("Напиши 2-е число.\n"); // сообщение к пользователю на ввод информации
+            int numb2 = in.nextInt(); // вот сюда эта информация сохраняется
+            int resultOutcome; // создаю переменную результат
+            System.out.print("Действие: "); // Вывод сообщения на экран, что будем делать: делить или умножать
+            String action = in.nextLine(); // вот сюда мы помещаем символ математической операции
+            action = in.nextLine();
 
-        //Дальше использовал схему switch\case — здесь удобнее чем прописывать всё в операторе if
+            //Дальше использовал схему switch\case — здесь удобнее чем прописывать всё в операторе if
 
-        switch (action) {
-            case "+": // Здесь всё просто: если action соответствует знаку +, то складываются две переменные. Дальше расписывать не буду
-                resultOutcome = numb1 + numb2;
-                System.out.print("Результат: " + resultOutcome);
-                break; // вот эта запись прекращает работу программы.
-            case "-":
-                resultOutcome = numb1 - numb2;
-                System.out.print("Результат: " + resultOutcome);
-                break;
-            case "*":
-                resultOutcome = numb1 * numb2;
-                System.out.print("Результат: " + resultOutcome);
-                break;
-            case "/": //Здесь применил if, потому что появилось условие
-                if (numb2 == 0) {
-                    System.out.print("Ошибка: на ноль делить может только Чак Норрис.");
-                } else {
-                    resultOutcome = numb1 / numb2;
+            switch (action) {
+                case "+": // Здесь всё просто: если action соответствует знаку +, то складываются две переменные. Дальше расписывать не буду
+                    resultOutcome = numb1 + numb2;
                     System.out.print("Результат: " + resultOutcome);
-                }
-                break;
-            default: // Используется если пользователь поведёт себя вне рамок соответствия продукта. Например, поставит на шаге "Действие" процент.
-                System.out.print("Что-то пошло не так.\nПопробуй ещё раз, но с меньшими числами и без процентов.");
+                    break; // вот эта запись прекращает работу программы.
+                case "-":
+                    resultOutcome = numb1 - numb2;
+                    System.out.print("Результат: " + resultOutcome);
+                    break;
+                case "*":
+                    resultOutcome = numb1 * numb2;
+                    System.out.print("Результат: " + resultOutcome);
+                    break;
+                case "/": //Здесь применил if, потому что появилось условие
+                    if (numb2 == 0) {
+                        System.out.print("Ошибка: на ноль делить может только Чак Норрис.");
+                    } else {
+                        resultOutcome = numb1 / numb2;
+                        System.out.print("Результат: " + resultOutcome);
+                    }
+                    break;
+                default: // Используется если пользователь поведёт себя вне рамок соответствия продукта. Например, поставит на шаге "Действие" процент.
+                    System.out.print("Что-то пошло не так.\nПопробуй ещё раз, но с меньшими числами и без процентов.");
+            }
         }
     }
 }
