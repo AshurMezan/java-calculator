7664f1b25886f33abf7dc909db211a3d17e508a3
Lion 2517 <lion2517@yahoo.com>
2023-04-03 15:04:00 +0500

    Исправил "падение программы" — добавил на проверку цикл while

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -2,45 +2,53 @@ package Calculator; // Создал пакет
 
 import java.util.Scanner; // Импортировал класс Scanner для запроса данных от пользователя
 
-public class Main { //точка входа в документ
-    public static void main(String[] args) { // основное тело класса
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.print("Напиши 1-e число.\n"); // сообщение к пользователю на ввод информации
-            int numb1 = in.nextInt(); // вот сюда эта информация сохраняется
-            System.out.print("Напиши 2-е число.\n"); // сообщение к пользователю на ввод информации
-            int numb2 = in.nextInt(); // вот сюда эта информация сохраняется
-            int resultOutcome; // создаю переменную результат
-            System.out.print("Действие: "); // Вывод сообщения на экран, что будем делать: делить или умножать
-            String action = in.nextLine(); // вот сюда мы помещаем символ математической операции
-            action = in.nextLine();
+        System.out.print("Напиши 1-e число.\n");
+        while (!in.hasNextInt()) { // добавляем проверку на тип вводимых данных
+            System.out.print("Неверный формат ввода. Введите целое число.\n");
+            in.next();
+        }
+        int numb1 = in.nextInt();
+        in.nextLine(); // съедаем символ новой строки
 
-            //Дальше использовал схему switch\case — здесь удобнее чем прописывать всё в операторе if
+        System.out.print("Напиши 2-е число.\n");
+        while (!in.hasNextInt()) {
+            System.out.print("Неверный формат ввода. Введите целое число.\n");
+            in.next();
+        }
+        int numb2 = in.nextInt();
+        in.nextLine();
 
-            switch (action) {
-                case "+": // Здесь всё просто: если action соответствует знаку +, то складываются две переменные. Дальше расписывать не буду
-                    resultOutcome = numb1 + numb2;
+        int resultOutcome;
+        System.out.print("Действие: ");
+        String action = in.nextLine();
+
+        switch (action) {
+            case "+":
+                resultOutcome = numb1 + numb2;
+                System.out.print("Результат: " + resultOutcome);
+                break;
+            case "-":
+                resultOutcome = numb1 - numb2;
+                System.out.print("Результат: " + resultOutcome);
+                break;
+            case "*":
+                resultOutcome = numb1 * numb2;
+                System.out.print("Результат: " + resultOutcome);
+                break;
+            case "/":
+                if (numb2 == 0) {
+                    System.out.print("Ошибка: на ноль делить может только Чак Норрис.");
+                } else {
+                    resultOutcome = numb1 / numb2;
                     System.out.print("Результат: " + resultOutcome);
-                    break; // вот эта запись прекращает работу программы.
-                case "-":
-                    resultOutcome = numb1 - numb2;
-                    System.out.print("Результат: " + resultOutcome);
-                    break;
-                case "*":
-                    resultOutcome = numb1 * numb2;
-                    System.out.print("Результат: " + resultOutcome);
-                    break;
-                case "/": //Здесь применил if, потому что появилось условие
-                    if (numb2 == 0) {
-                        System.out.print("Ошибка: на ноль делить может только Чак Норрис.");
-                    } else {
-                        resultOutcome = numb1 / numb2;
-                        System.out.print("Результат: " + resultOutcome);
-                    }
-                    break;
-                default: // Используется если пользователь поведёт себя вне рамок соответствия продукта. Например, поставит на шаге "Действие" процент.
-                    System.out.print("Что-то пошло не так.\nПопробуй ещё раз, но с меньшими числами и без процентов.");
-            }
+                }
+                break;
+            default:
+                System.out.print("Что-то пошло не так.\nПопробуй ещё раз, но с меньшими числами и без процентов.");
         }
     }
 }
